283e5afb20eac0427d551fa61bbe0db2ba52376e
md. Akther hossine <89641660+Akther985@users.noreply.github.com>
2022-09-18 01:29:47 -0700

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,18 @@
+(function($){
+
+  $('.testmonial-slider').slick({
+    autoplay: {
+      delay: 3000,
+      },
+    slidesToShow: 1,
+    arrows: true,
+    dots: false,
+    
+  
+   
+      
+    
+  });
+  
+  
+  })(jQuery);
